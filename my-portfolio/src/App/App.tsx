1aaee65a9e3b6a1bd79643eb3e1d796d448e501a
Alexander <renyreny125tt@gmail.com>
2022-06-14 15:32:30 +0300

    init: start project my portfolio

---
+++ my-portfolio/src/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Main from '../components/main/Main'
+import styles from './App.module.scss'
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <Main />
+    </div>
+  )
+}
+
+export default App
