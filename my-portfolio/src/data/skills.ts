65eccfaa0fc64331767f4bd40bf45b15e86b8cd8
Alexander <renyreny125tt@gmail.com>
2022-06-15 23:27:56 +0300

    feat: add skills block

---
+++ my-portfolio/src/data/skills.ts
@@ -0,0 +1,37 @@
+const skills = {
+  Base: {
+    JavaScript: 90,
+    TypeScript: 90,
+    GoogleSearch: 90,
+    EnglishWritten: 60,
+    EnglishSpoken: 40,
+    Git: 70,
+    Jest: 60,
+    SuperTest: 60,
+    TestingLibrary: 60,
+  },
+  FrontEnd: {
+    React: 90,
+    Redux: 90,
+    HTML: 90,
+    Css: 90,
+    Sass: 90,
+    Bem: 90,
+    Gulp: 70,
+    Webpack: 70,
+    Figma: 90,
+  },
+  BackEnd: {
+    NodeJS: 90,
+    Express: 90,
+    Fastify: 90,
+    NestJS: 90,
+    MongoDB: 70,
+    PostgreSQL: 70,
+    TypeORM: 70,
+    Mongoose: 70,
+    Docker: 70,
+  },
+}
+
+export default skills
