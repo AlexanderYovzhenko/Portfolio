81e6975ff742279a12501cb610d2d5e4113ed33a
Alexander <renyreny125tt@gmail.com>
2022-06-17 19:31:36 +0300

    refactor: change file

--- my-portfolio/src/data/experience.ts
+++ my-portfolio/src/data/experience.ts
@@ -2,7 +2,7 @@ const experienceStart = {
   JavaScript: '2020-11-17',
   NodeJS: '2021-07-03',
   React: '2021-12-10',
-  PetProjects: '2020-11-17',
+  'Pet Projects': '2020-11-17',
   Freelance: '2022-02-12',
 }
 
