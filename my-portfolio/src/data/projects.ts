e4c50b69eed3ea74840983bd7fc5a021256ba8ad
Alexander <renyreny125tt@gmail.com>
2022-06-22 22:00:28 +0300

    feat: add projects block

---
+++ my-portfolio/src/data/projects.ts
@@ -0,0 +1,62 @@
+import FancyWeather from '../components/projects/assets/FancyWeather.jpg'
+
+const projects = [
+  {
+    name: 'Fancy Weather',
+    deploy: 'https://goldenmanbel.github.io/Fancy-weather/',
+    screenshot: FancyWeather,
+    gitHub: 'https://github.com/GoldenManBel/Fancy-weather',
+    description: {
+      Description: 'Web application with the weather forecast',
+      Languages: 'JavaScript',
+      Technologies: 'HTML, CSS, Sass',
+      Instruments: 'Visual Studio Code, Chrome tools, Git, WebPack, ESLint',
+      Position: 'FrontEnd developer',
+      Role: 'I completed the whole project from start to finish',
+    },
+  },
+  {
+    name: 'Vezha',
+    deploy: 'https://goldenmanbel.github.io/Fancy-weather/',
+    screenshot: FancyWeather,
+    gitHub: 'https://github.com/GoldenManBel/Fancy-weather',
+    description: {
+      Description: 'Web application with the weather forecast',
+      Languages: 'JavaScript',
+      Technologies: 'HTML, CSS, Sass',
+      Instruments: 'Visual Studio Code, Chrome tools, Git, WebPack, ESLint',
+      Position: 'FrontEnd developer',
+      Role: 'I completed the whole project from start to finish',
+    },
+  },
+  {
+    name: 'Vezha',
+    deploy: 'https://goldenmanbel.github.io/Fancy-weather/',
+    screenshot: FancyWeather,
+    gitHub: 'https://github.com/GoldenManBel/Fancy-weather',
+    description: {
+      Description: 'Web application with the weather forecast',
+      Languages: 'JavaScript',
+      Technologies: 'HTML, CSS, Sass',
+      Instruments: 'Visual Studio Code, Chrome tools, Git, WebPack, ESLint',
+      Position: 'FrontEnd developer',
+      Role: 'I completed the whole project from start to finish',
+    },
+  },
+  {
+    name: 'Vezha',
+    deploy: 'https://goldenmanbel.github.io/Fancy-weather/',
+    screenshot: FancyWeather,
+    gitHub: 'https://github.com/GoldenManBel/Fancy-weather',
+    description: {
+      Description: 'Web application with the weather forecast',
+      Languages: 'JavaScript',
+      Technologies: 'HTML, CSS, Sass',
+      Instruments: 'Visual Studio Code, Chrome tools, Git, WebPack, ESLint',
+      Position: 'FrontEnd developer',
+      Role: 'I completed the whole project from start to finish',
+    },
+  },
+]
+
+export default projects
