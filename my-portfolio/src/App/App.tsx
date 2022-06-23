24543a1fd2ac673f43becadc0c106cbeccdeaeaf
Alexander <renyreny125tt@gmail.com>
2022-06-23 19:11:47 +0300

    feat: add projects block

--- my-portfolio/src/App/App.tsx
+++ my-portfolio/src/App/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import About from '../components/about/About'
 import Education from '../components/education/Education'
 import Experience from '../components/experience/Experience'
+import Projects from '../components/projects/Projects'
 import Skills from '../components/skills/Skills'
 import styles from './App.module.scss'
 
@@ -12,6 +13,7 @@ function App() {
       <Skills />
       <Education />
       <Experience />
+      <Projects />
     </div>
   )
 }
