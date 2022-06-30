13a1f1d4dd3b225a3098c2b2d272401e0015fb2e
Alexander <renyreny125tt@gmail.com>
2022-06-30 22:25:38 +0300

    refactor: add settings

--- my-portfolio/src/index.tsx
+++ my-portfolio/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import './index.scss'
 import App from './App/App'
 import { Provider } from 'react-redux'
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </React.StrictMode>
   </Provider>
 )
--- my-portfolio/src/utilities/i18next.ts
+++ my-portfolio/src/utilities/i18next.ts
@@ -1,14 +1,12 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
-// import HttpApi from 'i18next-http-backend'
 import translationRu from './translationRu'
 import translationEn from './translationEn'
 
 i18n
   .use(initReactI18next)
   .use(LanguageDetector)
-  // .use(HttpApi)
   .init({
     supportedLngs: ['en', 'ru'],
     fallbackLng: 'en',
@@ -23,14 +21,6 @@ i18n
         translation: translationRu,
       },
     },
-    // backend: {
-    //   loadPath:
-    //     'C:UsersGolden ManDesktopProjectsMyPortfoliomy-portfolio/public/assets/locales/{{lng}}/translation.json',
-    //   // 'https://goldenmanbel.github.io/project-management-app/assets/locales/{{lng}}/translation.json',
-    //   requestOptions: {
-    //     cache: 'no-store',
-    //   },
-    // },
   })
 
 export default i18n
